d185abda765dba05a01bf278eb350b717d579410
Cool <ozzz.grisha@yandex.ru>
2021-08-23 21:13:29 +0300

    MainSheet

--- app/src/main/java/com/example/jobik/SheetActivity.kt
+++ app/src/main/java/com/example/jobik/SheetActivity.kt
@@ -3,18 +3,15 @@ package com.example.jobik
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.jobik.Home.sheet.BottomSheetFragment
 
 
 class SheetActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.seet_main)
-
-        val btn = findViewById<Button>(R.id.button_main)
-        btn.setOnClickListener {
-            val bottomSheetFragment = BottomSheetFragment()
-            bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
-        }
     }
+
+    val btn = findViewById<Button>(R.id.button_main)
+
+
 }
